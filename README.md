2579a13ce8f3759d29278a179eb081ebc3b02404
Craig Whiteside <upperfoot@gmail.com>
2019-10-28 11:58:16 +0000

    Added signed url as its own function, updated README.md

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
+# Package's purpose
+
+This is to prevent lightweight services from having to intake file data in order to store the files, instead we offload this work to google and we simply reference the files stored by an App or third party service.
+
+# Use cases
+
+1. Uploading a file to be saved in a Database using item.name as a reference
+2. Uploading a file to be processed by a consuming service
+
 # Creating a Signed Temporary Upload
 
+- Bucket Name = Google Storage Bucket Name
+- Project ID = Google Project ID
+- Storage Credentials = Google Service Account JSON credentials
+
 ```
+// First Argument Bucket Name
+// Second Argument Project ID
+// Third Argument Storage Credentials - File name in current working directory
 const uploadService = new GoogleCloudFileService('bucket-name', 'project-id', 'storage-credentials.json');
 
-// First Argument - Content Type, Second argument Expiry time in microseconds
+// First Argument - Content Type
+// Second argument Expiry time in microseconds
 uploadService.createSignedUpload('image/jpeg', Date.now() + 1000000).then((signedUpload : SignedUpload) => {
     // This returns a SignedUpload object containing;
     return {
+        signedUpload.fileName, // the fileName to use as reference when storing the temporary file permanently
+        signedUpload.contentType // the file content type used to generate this link
+        signedUpload.expiryTime, // the expiry time used to generate this link
         signedUpload.writeUrl, // the write url for uploading the file via a PUT HTTP request
-        signedUpload.readUrl, // the read url for when a file has been uploaded
-        signedUpload.fileName // the fileName to use as reference when storing the temporary file permanently
+        signedUpload.readUrl, // the read url for when a file has been uploaded GET HTTP request
     }
 });
 
@@ -20,7 +39,19 @@ uploadService.createSignedUpload('image/jpeg', Date.now() + 1000000).then((signe
 ```
 // First argument (fileName generated when using createSignedUpload)
 // Second argument (destination folder)
-uploadService.storeTemporaryFile(item.fileName, 'proof').then((item: storage.File) => {
+uploadService.storeTemporaryFile(item.fileName, 'proof').then((file: storage.File) => {
     // Save item.name to DB as an example
 });
 ```
+
+# Making files public or private
+
+By default files are made private unless specifically set to public, you would achieve this by;
+
+```
+// storage.File object from storeTemporaryFile above or getFile method
+file.makePublic()
+```
+
+# Further Reading
+- https://googleapis.dev/nodejs/storage/latest/global.html
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import * as storage from '@google-cloud/storage';
 import * as uuid from 'uuid/v4';
 import * as mime from 'mime-types';
+import { GetSignedUrlResponse } from '@google-cloud/storage';
 
 export class SignedUpload {
     public fileName: string;
+    public contentType: string;
+    public expiryTime: number;
     public writeUrl: string;
     public readUrl : string;
 
-    constructor(fileName: string, writeUrl: string, readUrl: string) {
+    constructor(fileName: string, contentType: string, expiryTime: number, writeUrl: string, readUrl: string) {
         this.fileName = fileName;
+        this.contentType = contentType;
+        this.expiryTime = expiryTime;
         this.writeUrl = writeUrl;
         this.readUrl = readUrl;
     }
@@ -40,29 +45,34 @@ export class GoogleCloudFileService {
         const extension = mime.extension(contentType);
         return uniqueString + "." + extension;
     }
+
+    async createSignedUrl(file: storage.File, signedUrlConfig : storage.GetSignedUrlConfig) : Promise<string> {
+        const response : GetSignedUrlResponse = await file.getSignedUrl(signedUrlConfig);
+        return response[0];
+    }
     
-    async createSignedUpload(contentType: string, expires: number) : Promise<SignedUpload> {
+    async createSignedUpload(contentType: string, expiryTime: number) : Promise<SignedUpload> {
         // Create Temporary File in Temporary Folder
         const temporaryFileName = this.makeFileName(contentType);
         const temporaryFilePath = this.TEMPORARY_FOLDER + "/" + temporaryFileName;
         const file = this.getFile(temporaryFilePath);
-        
+
         const writeConfig = {
           action: "write",
-          expires,
+          expires: expiryTime,
           contentType
         }  as storage.GetSignedUrlConfig;
-        
-        const signedWriteUrl = await file.getSignedUrl(writeConfig);
-        
-        const readConfig = {
-          action: "read",
-          expires
-        }  as storage.GetSignedUrlConfig;
-        
-        const signedReadUrl = await file.getSignedUrl(readConfig);
 
-        return new SignedUpload(temporaryFileName, signedWriteUrl[0], signedReadUrl[0]);
+        const signedWriteUrl = await this.createSignedUrl(file, writeConfig);
+
+        const readConfig = {
+            action: "read",
+            expires: expiryTime
+        }  as storage.GetSignedUrlConfig;
+
+        const signedReadUrl = await this.createSignedUrl(file, readConfig);
+
+        return new SignedUpload(temporaryFileName, contentType, expiryTime, signedWriteUrl, signedReadUrl);
     }
     
     async storeTemporaryFile(temporaryFileName: string, destinationFolder: string) : Promise<storage.File> {
